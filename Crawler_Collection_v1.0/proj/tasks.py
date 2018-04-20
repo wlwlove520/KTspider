ae097b00bfad79d613f3bc383c2a0954ac63062c
wlwlove520 <274049441@qq.com>
2018-04-20 18:19:42 +0800

    Delete tasks.py

--- Crawler_Collection_v1.0/proj/tasks.py
+++
@@ -1,8 +0,0 @@
-from celeryrun import app
-import subprocess
-
-@app.task
-def run_10min():
-    runner = subprocess.Popen('python3 /home/linhanqiu/crawler240/KTspider/run.py',shell=True)
-    runner.wait()
-    return "end"
