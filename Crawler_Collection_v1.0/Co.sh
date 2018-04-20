67bae8c750cb1d196f103a9c2bb7c6a0bca9b64d
wlwlove520 <274049441@qq.com>
2018-04-20 18:20:36 +0800

    Delete Co.sh

--- Crawler_Collection_v1.0/Co.sh
+++
@@ -1,3 +0,0 @@
-docker run -v /home/Code/WULEWEI/Crawler_Collection/KTspider:/usr/src/wlw_test  -w /usr/src/wlw_test python_spider:spider_v1.2 python run.py && \cp /home/Code/WULEWEI/Crawler_Collection/KTspider/cp_data/data/* /home/admin/spider2flume/data/ && \cp /home/Code/WULEWEI/Crawler_Collection/KTspider/cp_data/logs/* /home/admin/spider2flume/log/ && mv /home/Code/WULEWEI/Crawler_Collection/KTspider/cp_data/data/* /home/Code/WULEWEI/Crawler_Collection/KTspider/data/ && mv /home/Code/WULEWEI/Crawler_Collection/KTspider/cp_data/logs/* /home/Code/WULEWEI/Crawler_Collection/KTspider/log/logs/
-
-
